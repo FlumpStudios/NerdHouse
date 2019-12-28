76b25f4cfb3c2eb9189075ef46c184abdcba66be
Paul <flumpstudios@gmail.com>
2019-12-28 23:53:12 +0000

    Removed blog section; Hide in navigation option added

--- home/models.py
+++ home/models.py
@@ -13,7 +13,6 @@ from wagtail.snippets.models import register_snippet
 from wagtail.search import index
 from modelcluster.fields import ParentalKey
 
-
 class carouselOrderable(Orderable, models.Model):
     page = ParentalKey('HomePage', on_delete=models.CASCADE, related_name='carouselOrderable')
     
@@ -98,6 +97,22 @@ class testimonialOrderable(Orderable, models.Model):
     def __str__(self):
         return self.page.title + " -> " + self.gallery.name
 
+class socialMediaOrderable(Orderable, models.Model):
+    page = ParentalKey('HomePage', on_delete=models.CASCADE, related_name='socialMediaOrderable')
+    socialMediaItem = models.ForeignKey('socialMediaItem', on_delete=models.CASCADE, related_name='+')
+
+    class Meta:
+        verbose_name = "Social Media item"
+        verbose_name_plural = "Social Media items"
+
+    panels = [
+        SnippetChooserPanel('socialMediaItem'),
+    ]
+
+    def __str__(self):
+        return self.page.title + " -> " + self.gallery.name
+
+
 @register_snippet
 class gallery(models.Model):
     image = models.ForeignKey(
@@ -107,8 +122,9 @@ class gallery(models.Model):
         on_delete=models.SET_NULL,
         related_name='+'
     )
-    link = models.URLField(null=True, blank=True)
+    
     name = models.CharField(max_length=50)
+    link = models.URLField(null=True, blank=True)    
     description = models.CharField(max_length=50)
 
     panels = [
@@ -116,6 +132,22 @@ class gallery(models.Model):
         FieldPanel('link'),
         FieldPanel('name'),
         FieldPanel('description')
+    ]
+
+    def __str__(self):
+        return self.name
+
+
+@register_snippet
+class socialMediaItem(models.Model):
+    name = models.CharField(max_length=50)
+    link = models.URLField(null=True, blank=True)    
+    icon = models.CharField(max_length=50)
+
+    panels = [
+        FieldPanel('link'),
+        FieldPanel('name'),
+        FieldPanel('icon')
     ]
 
     def __str__(self):
@@ -193,8 +225,7 @@ class HomePage(Page):
     client_postcode = models.CharField(max_length=50, default="XX00 0XX ", null=True)
     client_email = models.CharField(max_length=50, default="joe@bloggs.com", null=True)
     client_phone = models.CharField(max_length=50, default="01234 456789", null=True)
-    client_moblie = models.CharField(max_length=50, default="01234 456789", null=True)
-    
+    client_moblie = models.CharField(max_length=50, default="01234 456789", null=True)    
     
     blurb_header = models.CharField(max_length=50, default="Blurb")    
     blurb_subheader = models.CharField(max_length=50, default="this is a blurb ")
@@ -202,31 +233,36 @@ class HomePage(Page):
     blurb_buttonText = models.CharField(max_length=50, default="Find out more")
     blurb_link = models.URLField(null=True, blank=True)
     show_blurb = models.BooleanField(default=True)
+    show_blurb_in_navigation = models.BooleanField(default=False)
 
     image_gallery_title = models.CharField(max_length=50, default="Gallery")  
     image_gallery_text = RichTextField(blank=True)
+    show_gallery_in_navigation = models.BooleanField(default=True)
 
     second_blurb_header = models.CharField(max_length=50, default="Blurb 2")    
     second_blurb_subheader = models.CharField(max_length=50, default="My Second Blurb 2")
     second_blurb_text = RichTextField(blank=True)
-    show_second_blurb = models.BooleanField(default=True)
-
     second_blurb_buttonText = models.CharField(max_length=50, default="Find out more")
     second_blurb_link = models.URLField(null=True, blank=True)
+    show_second_blurb = models.BooleanField(default=True)
+    show_second_blurb_in_navigation = models.BooleanField(default=False)
  
-    enumerated_list_header = models.CharField(max_length=50, default="Approach")    
+    enumerated_list_header = models.CharField(max_length=50, default="Approach")
+    show_enumerated_list_in_navigation = models.BooleanField(default=True)    
 
     icon_list_header = models.CharField(max_length=50, default="Services")    
-    
-    testimonials_header = models.CharField(max_length=50, default="Testimonials")    
+    show_icon_list_in_navigation = models.BooleanField(default=True)
+
+    testimonials_header = models.CharField(max_length=50, default="Testimonials")
+    show_testimonials_in_navigation = models.BooleanField(default=False) 
 
     contact_title = models.CharField(max_length=50, default="Contact")        
     contact_subtitle = models.CharField(max_length=50, default="Get in contact")    
     show_contact_form = models.BooleanField(default=True)
+    show_contact_in_navigation = models.BooleanField(default=True)
 
     footer_info_title = models.CharField(max_length=50, default="About") 
-    footer_info = RichTextField(blank=True)      
-    
+    footer_info = RichTextField(blank=True)    
     
     intro_background = models.ForeignKey(
         'wagtailimages.Image',
@@ -258,7 +294,9 @@ class HomePage(Page):
                 FieldPanel('client_postcode'),  
                 FieldPanel('client_email'),          
                 FieldPanel('client_phone'),
-                FieldPanel('client_moblie')
+                FieldPanel('client_moblie'),
+                InlinePanel('socialMediaOrderable', label="Social Media Item")
+
             ],
             heading="Personal Information",  
         ),
@@ -285,7 +323,8 @@ class HomePage(Page):
                 ImageChooserPanel('blurb_background'),
                 FieldPanel('blurb_buttonText'),
                 FieldPanel('blurb_link'),
-                FieldPanel('show_blurb')
+                FieldPanel('show_blurb'),
+                FieldPanel('show_blurb_in_navigation')
             ],
             heading="Blurb",  
         ),
@@ -296,7 +335,8 @@ class HomePage(Page):
             [
                 FieldPanel('image_gallery_title'),
                 FieldPanel('image_gallery_text'),                
-                InlinePanel('galleryOrderable', label="Gallery Item")
+                InlinePanel('galleryOrderable', label="Gallery Item"),
+                FieldPanel('show_gallery_in_navigation')
             ],
             heading="Gallery",  
         ),
@@ -311,7 +351,8 @@ class HomePage(Page):
                 InlinePanel('carouselOrderable', label="Carousel Image"),
                 FieldPanel('second_blurb_buttonText'),
                 FieldPanel('second_blurb_link'),
-                FieldPanel('show_second_blurb')
+                FieldPanel('show_second_blurb'),
+                FieldPanel('show_second_blurb_in_navigation')
             ],
             heading="Second Blurb",  
         ),
@@ -321,7 +362,8 @@ class HomePage(Page):
         MultiFieldPanel(
             [
                 FieldPanel('enumerated_list_header'),                
-                InlinePanel('enumeratedListOrderable', label="Enumerated List Item")
+                InlinePanel('enumeratedListOrderable', label="Enumerated List Item"),
+                FieldPanel('show_enumerated_list_in_navigation')
             ],
             heading="Enumerated List",  
         ),
@@ -331,7 +373,8 @@ class HomePage(Page):
         MultiFieldPanel(
             [
                 FieldPanel('icon_list_header'),                
-                InlinePanel('iconListOrderable', label="Icon List Item")
+                InlinePanel('iconListOrderable', label="Icon List Item"),
+                FieldPanel('show_icon_list_in_navigation')
             ],
             heading="Icon List",  
         ),
@@ -341,7 +384,8 @@ class HomePage(Page):
         MultiFieldPanel(
             [
                 FieldPanel('testimonials_header'),                
-                InlinePanel('testimonialOrderable', label="Testimonial Item")
+                InlinePanel('testimonialOrderable', label="Testimonial Item"),
+                FieldPanel('show_testimonials_in_navigation')
             ],
             heading="Icon List",  
         ),
@@ -352,7 +396,8 @@ class HomePage(Page):
             [
                 FieldPanel('contact_title'),                
                 FieldPanel('contact_subtitle'),
-                FieldPanel('show_contact_form')
+                FieldPanel('show_contact_form'),
+                FieldPanel('show_contact_in_navigation')
             ],
             heading="Contact Form",  
         ),
