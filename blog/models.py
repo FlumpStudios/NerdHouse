93790c048104f685ca53a7ad0ca413553c62b784
Paul <flumpstudios@gmail.com>
2020-01-04 00:36:53 +0000

    Added gravitar image to blog pages

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 from django.db import models
+import hashlib
 
 # New imports added for ParentalKey, Orderable, InlinePanel, ImageChooserPanel
 
@@ -30,6 +31,7 @@ class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     author = models.CharField(max_length=50, default="Joe Blogs")
+    authors_email = models.EmailField(max_length=50, null=True)
     body = RichTextField(blank=True)
     caption = models.CharField(blank=True, max_length=250)
     main_image = models.ForeignKey(
@@ -39,7 +41,19 @@ class BlogPage(Page):
         on_delete=models.SET_NULL, 
         related_name='+'
     )
-   
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        stri = str(self.authors_email).lower().strip()
+        result = hashlib.md5(stri.encode())
+        gravitarSrc = result.hexdigest()
+        context['gravitarSrc'] = gravitarSrc
+        return context
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]   
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
@@ -50,6 +64,7 @@ class BlogPage(Page):
         FieldPanel('date'),
         FieldPanel('intro'),
         FieldPanel('author'),
+        FieldPanel('authors_email'),
         FieldPanel('body', classname="full"),
         ImageChooserPanel('main_image'),
         FieldPanel('caption')
