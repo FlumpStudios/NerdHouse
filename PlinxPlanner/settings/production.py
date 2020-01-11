8ad6d7a2a9e643c1ac7c08e5c4c1fd35f581a76b
Paul Marrable <flumpstudios@gmail.com>
2020-01-11 10:01:53 +0000

    Added production settings

--- PlinxPlanner/settings/production.py
+++ PlinxPlanner/settings/production.py
@@ -1,6 +1,22 @@
+from __future__ import absolute_import, unicode_literals
 from .base import *
+import os
 
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+
+
+
 
 try:
     from .local import *
